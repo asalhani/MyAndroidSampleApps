89f436f51c69458cadcdd7a73b2844421805bd8d
Adib Ahmad <asalhani@outlook.com>
2017-08-18 19:52:57 +0300

    my solution

--- SimpleLoginForm/MyApplication3/app/src/main/java/com/example/adeeb/simpleloginformapp/MainActivity.java
+++ SimpleLoginForm/MyApplication3/app/src/main/java/com/example/adeeb/simpleloginformapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.adeeb.simpleloginformapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View view){
+        EditText txtUserName = (EditText)findViewById(R.id.txtUserName);
+        EditText txtPassword = (EditText)findViewById(R.id.txtPassword);
+
+        Log.i("Entered Values:", txtUserName.getText() + " - " + txtPassword.getText());
     }
 }
